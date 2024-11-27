64fd4ff5bdb0e3caf17b6cf5b2bef4415854739f
Marcela <ardilamarcela42@gmail.com>
2024-11-27 15:41:14 -0500

    cors

--- src/infrastructure/server.express.ts
+++ src/infrastructure/server.express.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import router from './routes/index.routes';
 import middleware404 from './middlewares/middlewares';
-
+import cors from 'cors'
 
 export const createServerExpress = () => {
     const app = express();
-    
+    app.use(cors())
    app.use(express.json());
    app.use(router);
    app.use(middleware404)
